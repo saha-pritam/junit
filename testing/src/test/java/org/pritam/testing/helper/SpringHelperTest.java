bdd814fd3fc8087416b83c1030c89d57d54d1eeb
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-16 19:38:01 +0530

    Topic 5.

--- testing/src/test/java/org/pritam/testing/helper/SpringHelperTest.java
+++ testing/src/test/java/org/pritam/testing/helper/SpringHelperTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 public class SpringHelperTest {
 
 	@Test
-	public void test() {
-		System.out.println("This is just a basic test case. No failure means test case succeeded");
+	public void somethingElse() {
+		System.out.println("This is just a basic test case. No failure means test case succeeded and also method name is changed from test to somethingElse.");
 	}
 
 }
