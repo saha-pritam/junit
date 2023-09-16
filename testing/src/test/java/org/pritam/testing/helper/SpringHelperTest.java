e204b4992d67f55ddae066a1017489421a29ba92
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-16 19:47:41 +0530

    Topic 8.

--- testing/src/test/java/org/pritam/testing/helper/SpringHelperTest.java
+++ testing/src/test/java/org/pritam/testing/helper/SpringHelperTest.java
@@ -1,14 +1,36 @@
 package org.pritam.testing.helper;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class SpringHelperTest {
 
+	StringHelper stringHelper = new StringHelper();
+	
 	@Test
-	public void somethingElse() {
-		fail("Failing intentionally");
+	public void testTruncateAInFirst2Positions1() {
+		assertEquals("",stringHelper.truncateAInFirst2Positions("A"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions2() {
+		assertEquals("",stringHelper.truncateAInFirst2Positions("AA"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions3() {
+		assertEquals("CD",stringHelper.truncateAInFirst2Positions("AACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions4() {
+		assertEquals("CDAA",stringHelper.truncateAInFirst2Positions("CDAA"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions5() {
+		assertEquals("ACD",stringHelper.truncateAInFirst2Positions("AACD"));
 	}
 
 }
