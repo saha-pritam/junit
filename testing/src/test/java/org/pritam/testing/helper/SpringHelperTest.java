0a9a5b95268ef77d0a9bedaf2a0a37d583574c0c
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-16 19:39:51 +0530

    Topic 7.

--- testing/src/test/java/org/pritam/testing/helper/SpringHelperTest.java
+++ testing/src/test/java/org/pritam/testing/helper/SpringHelperTest.java
@@ -1,12 +1,14 @@
 package org.pritam.testing.helper;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class SpringHelperTest {
 
 	@Test
 	public void somethingElse() {
-		System.out.println("This is just a basic test case. No failure means test case succeeded and also method name is changed from test to somethingElse.");
+		fail("Failing intentionally");
 	}
 
 }
