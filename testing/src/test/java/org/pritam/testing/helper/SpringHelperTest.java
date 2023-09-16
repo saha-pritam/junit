a109af71c99d81c9e2f0b73d410d8fba83ebf922
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-16 19:35:31 +0530

    Topic 3 and 4.

---
+++ testing/src/test/java/org/pritam/testing/helper/SpringHelperTest.java
@@ -0,0 +1,12 @@
+package org.pritam.testing.helper;
+
+import org.junit.Test;
+
+public class SpringHelperTest {
+
+	@Test
+	public void test() {
+		System.out.println("This is just a basic test case. No failure means test case succeeded");
+	}
+
+}
