964b5badd463ada6ef7cd47f522a686e494583d3
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-21 12:43:36 +0530

    Topic-20.

---
+++ testing/src/test/java/org/pritam/testing/general/TestSuiteDemo.java
@@ -0,0 +1,11 @@
+package org.pritam.testing.general;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ArrayTesting.class, ExceptionTesting.class })
+public class TestSuiteDemo {
+
+}
