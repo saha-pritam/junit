b303a7815560e95d9ec751091a48d48bdf3f4a6e
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-22 20:04:56 +0530

    Topic-39.

---
+++ testing/src/test/java/org/pritam/testing/mockito/AtLeastMethodDemo.java
@@ -0,0 +1,47 @@
+package org.pritam.testing.mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.pritam.testing.business.TodoBusinessImpl;
+import org.pritam.testing.data.api.TodoService;
+
+public class AtLeastMethodDemo {
+
+	@Test
+	public void test1() {
+		//Given
+		TodoService todoService = mock(TodoService.class);
+		given(todoService.retrieveTodos(anyString())).willReturn(Arrays.asList("Learn Spring MVC","Learn Spring", "Learn To Dance"));
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		
+		//When
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Pritam");
+		
+		//Then
+		verify(todoService, atLeast(1)).deleteTodo(anyString());
+	}
+	
+	@Test
+	public void test2() {
+		//Given
+		TodoService todoService = mock(TodoService.class);
+		given(todoService.retrieveTodos(anyString())).willReturn(Arrays.asList("Learn Spring MVC","Learn Spring", "Learn To Dance"));
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		
+		//When
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Pritam");
+		
+		//Then
+		verify(todoService, atLeast(2)).deleteTodo(anyString());
+	}
+
+}
