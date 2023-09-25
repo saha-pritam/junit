4f42e395842628b714307c891c7ab656eca9c552
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-25 17:11:14 +0530

    Topic-47,48.

---
+++ testing/src/test/java/org/pritam/testing/mockitoAnnotations/TodoBusinessImplTestingWithMockitoAnnotation.java
@@ -0,0 +1,36 @@
+package org.pritam.testing.mockitoAnnotations;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.pritam.testing.business.TodoBusinessImpl;
+import org.pritam.testing.data.api.TodoService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TodoBusinessImplTestingWithMockitoAnnotation {
+	
+	@Mock
+	private TodoService todoService;
+
+	@Test
+	public void testRetrieveTodosRelatedToSpring_withStub_1() {
+		given(todoService.retrieveTodos("Pritam")).willReturn(Arrays.asList("Learn Spring MVC","Learn Spring", "Learn To Dance"));
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		assertEquals(2, todoBusinessImpl.retrieveTodosRelatedToSpring("Pritam").size());
+	}
+	
+	@Test
+	public void testRetrieveTodosRelatedToSpring_withStub_2() {
+		given(todoService.retrieveTodos("Pritam")).willReturn(Arrays.asList("Learn Spring MVC","Learn Spring", "Learn To Dance"));
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		String arr[] = {"Learn Spring MVC","Learn Spring"};
+		assertArrayEquals(arr, todoBusinessImpl.retrieveTodosRelatedToSpring("Pritam").toArray());
+	}
+
+}
